2e01ca3e60676f6ce7bdb57efbddaa2535bde57b
余志成 <1029686739@qq.com>
2021-08-12 18:51:31 +0800

    feat(state): :sparkles:  学习react state的更新机制

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import { OriginalIndex, FilterElemDome } from './demos/jsx';
-import ReactComponentDemo from './demos/component';
+// import { OriginalIndex, FilterElemDome } from './demos/jsx';
+// import ReactComponentDemo from './demos/component';
+// state
 import StateDemo from './demos/state/index';
 import './App.css';
 
 function App() {
   return (
     <>
-      <OriginalIndex />
+      {/* <OriginalIndex />
       <FilterElemDome />
-      <ReactComponentDemo />
+      <ReactComponentDemo /> */}
       <h2>state</h2>
       <StateDemo />
     </>
--- src/demos/state/index.js
+++ src/demos/state/index.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-export default function FunctionComp() {
+function FunctionComp() {
   const [count, setCount] = React.useState(0);
   const [msg, setMsg] = React.useState('hello ');
 
-  const handleClick = () => {
+  const handleClick = React.useCallback(() => {
+    // mock api
     setTimeout(() => {
-      let nextCount = count + 1;
       ReactDom.unstable_batchedUpdates(() => {
-        setCount(nextCount);
-        setCount(nextCount + 1);
-        setCount(nextCount + 2);
-        setMsg('text');
+        setTimeout(() => {
+          setCount(prevCount => prevCount + 1);
+        }, 100);
+        setCount(prevCount => prevCount + 1);
+        setCount(prevCount => prevCount + 1);
+        setCount(prevCount => prevCount + 1);
+        ReactDom.flushSync(() => {
+          setCount(prevCount => prevCount + 1);
+        });
+        setCount(prevCount => prevCount + 1);
+        setMsg('job');
       });
-    }, 1000);
-  };
+    });
+  }, []);
 
-  console.log('render');
+  console.log('render', count, msg);
   return (
     <div>
       <button onClick={handleClick}>
@@ -26,3 +33,125 @@ export default function FunctionComp() {
     </div>
   );
 }
+
+class ClassDome extends React.PureComponent {
+  state = {
+    count: 0,
+    number: 0,
+    num1: 0,
+    userInfo: {
+      name: 'tom',
+      age: 13,
+    },
+  };
+
+  handleClick = () => {
+    setTimeout(() => {
+      ReactDom.unstable_batchedUpdates(() => {
+        this.setState(prevState => {
+          return {
+            count: prevState.count + 1,
+          };
+        });
+        this.setState(prevState => {
+          return {
+            count: prevState.count + 1,
+          };
+        });
+        this.setState(prevState => {
+          return {
+            count: prevState.count + 1,
+          };
+        });
+      });
+    }, 1000);
+    // this.incrementCount();
+    // this.incrementCount();
+    // this.incrementCount();
+    // this.incrementCountBetter();
+    // this.incrementCountBetter();
+    // this.incrementCountBetter();
+  };
+
+  incrementCount = () => {
+    console.log(this.state.count);
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  incrementCountBetter = () => {
+    this.setState(prevState => {
+      return {
+        count: prevState.count + 1,
+      };
+    });
+  };
+
+  handleNumberClick = () => {
+    setTimeout(() => {
+      this.setState({ number: 1 });
+    });
+
+    this.setState({
+      number: 2,
+    });
+
+    ReactDom.flushSync(() => {
+      this.setState({
+        number: 3,
+      });
+    });
+    this.setState({
+      number: 4,
+    });
+  };
+
+  handleChangeUser = () => {
+    const user = this.state.userInfo;
+    user.age = 88;
+    this.setState(
+      {
+        userInfo: this.state.userInfo,
+      },
+      () => {
+        console.log('change userInfo after call cb');
+      }
+    );
+  };
+
+  componentDidUpdate() {
+    console.log('call componentDidUpdate');
+  }
+
+  render() {
+    console.log('call render ', this.state.number);
+
+    return (
+      <div>
+        <h3>class component demo</h3>
+        <button onClick={this.handleClick}>{this.state.count}</button>
+        <button onClick={this.handleNumberClick}>
+          click number:{this.state.count}
+        </button>
+
+        <div>
+          <p>
+            name:{this.state.userInfo.name},age:{this.state.userInfo.age}
+          </p>
+          <button onClick={this.handleChangeUser}>change user</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+const Demo = () => {
+  return (
+    <>
+      <FunctionComp />
+      <ClassDome />
+    </>
+  );
+};
+export default Demo;
